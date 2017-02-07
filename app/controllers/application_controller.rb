be14ee7002da1d1a2768c5daf1ae1eb04e22f2cc
Reggie <reggiest09@gmail.com>
2017-02-07 01:17:49 -0500

    gg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,28 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
+  before_filter :store_current_location, :unless => :devise_controller?
+
+
+ private
+  # override the devise helper to store the current location so we can
+  # redirect to it after loggin in or out. This override makes signing in
+  # and signing up work automatically.
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+
+private
+  # override the devise method for where to go after signing out because theirs
+  # always goes to the root path. Because devise uses a session variable and
+  # the session is destroyed on log out, we need to use request.referrer
+  # root_path is there as a backup
+  def after_sign_out_path_for(resource)
+    request.referrer || root_path
+  end
+
+
+
 
 
 
